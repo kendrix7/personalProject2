70eca8a3108b77c246377559143e79d9a543bb22
Mark Nielsen <marknielsen7@outlook.com>
2017-11-06 18:51:23 -0700

    contact page content

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,8 +4,22 @@ import './Contact.css';
 export default class Contact extends Component {
     render() {
         return (
-            <div>
-                Contact
+            <div className='contact_main_container'>
+                
+                <div className='contact_top_container'>
+                    <div className='contact_top_inner_box'>
+                        <h2>CONTACT</h2>
+                        <div className='contact_top_text_box'>
+                            <p className='contact_days'>MONDAY - FIRDAY</p><p>6pm - 8pm MST</p>
+                        </div>
+                        <div className='contact_top_text_box'>
+                            <p className='contact_days'>SATURDAY - SUNDAY</p><p>9am - 12pm MST</p>
+                        </div>
+                        <div className='contact_top_phone'>1 (801) 867-0109</div>
+                        <div className='contact_top_underline'></div>
+                    </div>
+                </div>
+                
             </div>
         )
     }
